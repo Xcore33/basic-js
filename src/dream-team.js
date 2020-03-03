318d3709ffe6031b2a76db8fbb56ac7a81775cf6
xcore33 <mauler1987@gmail.com>
2020-03-03 23:01:44 +0300

    feat: dream-team decision

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,9 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  const Names = members.filter(memberName => typeof memberName === 'string');
+  const trimNames = Names.map(item => item.trim()); //delete incorrect symbols
+  const dreamTeam = trimNames.map(item => item[0].toUpperCase());
+  return dreamTeam.sort().join('');
 };
